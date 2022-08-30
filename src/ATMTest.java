c9a34b66b58fef2b4347de486dcf803c2c8c1411
Steven Ko <stevenk8819@gmail.com>
2022-08-30 14:05:44 -0700

    test1

--- src/ATMTest.java
+++ src/ATMTest.java
@@ -1,7 +1,61 @@
-
+import java.util.*;
 public class ATMTest {
-	public static void main (String [] arasjflffsdfasdjfsalkfjsdaklfjdsaklfjsadklfjasdlkjsadlkf)
+
+	HashMap<Integer, Double> acc; 
+	public static void main (String [] monkeyMONKEYmonkey)
 	{
-		System.out.println ("fffffffff"); 
+		ATMTest t = new ATMTest(); 
+		t.openAccount(40); 
+		t.depositMoney(40,30.0); 
+		System.out.println (t.getA().get(40)); 
+		t.withdrawMoney(40,20.0);
+		System.out.println (t.getA().get(40)); 
+		
 	}
+	public ATMTest () {
+		acc = new HashMap<Integer, Double>(); 
+	}
+	private HashMap<Integer, Double> getA() {
+		return acc; 
+	}
+	public void openAccount(int accNumber) {
+		acc.put(accNumber, 0.0); 
+	}
+	public void openAccount(int accNumber, double dep) {
+		acc.put(accNumber, dep); 
+	}
+	public void closeAccount(int accNumber) {
+		double d = acc.get(accNumber); 
+		if (d < 0.0) {
+			acc.remove(accNumber); 
+		}
+	}
+	public double checkBalance(int acc1) {
+		if (acc.containsKey(acc1)) {
+			return acc.get(acc1); 
+		}
+		else {
+			return 0.0; 
+		}
+	}
+	public void depositMoney(int num, double dep) {
+		if (acc.containsKey(num)) {
+			double temp = acc.get(num); 
+			temp += dep; 
+			acc.replace(num, temp); 
+		}
+	}
+	public void withdrawMoney(int num, double with) {
+		if (acc.containsKey(num)) {
+			double temp = acc.get(num); 
+			if (with>temp) {
+				System.out.println ("YOU'RE BROKE HOMIE"); 
+				return; 
+			}
+			temp -= with; 
+			acc.replace(num, temp); 
+		}
+	}
+	
+	
 }
---
+++ src/Account.java
@@ -0,0 +1,12 @@
+
+public class Account {
+	private String id; 
+	private double balance; 
+	public Account(String id1, double balance1) {
+		id = id1; 
+		balance = balance1; 
+	}
+	public Account(String id1) {
+		id = id1; 
+	}
+}
